5ee34ecd408731270e6dbeeef4dc9e3ee497bd44
AreyBRSK <areybrsk@gmail.com>
2022-05-27 13:25:52 +0300

    Updated for Amalofire

--- WeatherApp/Services/NetworkManager.swift
+++ WeatherApp/Services/NetworkManager.swift
@@ -6,32 +6,42 @@
 //
 
 import Foundation
+import Alamofire
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
 
 class NetworkManager {
     static let shared = NetworkManager()
     
     private init() {}
     
-    func fetchWeather(for url: String, _ completion: @escaping(CurrentWeather, DailyWeather) -> Void) {
-        
+    func fetchWeather(from url: String, _ completion: @escaping(Result<Weather, NetworkError>) -> Void) {
         guard let url = URL(string: url) else { return }
         
-        URLSession.shared.dataTask(with: url) {data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "Unknown error has occurred.")
-                return
+        AF.request(url)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    guard let weatherData = value as? [String: Any] else { return }
+                    guard let currentWeatherData = weatherData["current_weather"] as? [String: Any] else { return }
+                    guard let dailyWeatherData = weatherData["daily"] as? [String: Any] else { return }
+                    
+                    let weather = Weather(
+                        currentWeather: CurrentWeather(from: currentWeatherData),
+                        daily: DailyWeather(from: dailyWeatherData)
+                    )
+                    
+                    completion(.success(weather))
+                case .failure:
+                    completion(.failure(.decodingError))
+                }
             }
-            
-            do {
-                let weather = try JSONDecoder().decode(Weather.self, from: data)
-                let currentWeather = weather.current_weather
-                let dailyWeather = weather.daily
-                completion(currentWeather, dailyWeather)
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-        }.resume()
     }
 }
+
 
